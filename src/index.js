d9ef5cb94d996678175dc7ea13ad170337219752
paula <p.valenciano.sausor@accenture.com>
2018-08-21 09:58:01 +0200

    develop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+//para aplicaciones web
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+//simulador de bbdd
 import registerServiceWorker from './registerServiceWorker';
 
+//reactDom -> enfocado a aplicacion web y la ejecuta
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
